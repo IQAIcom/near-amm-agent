57347ff6263372269a7f406ac5e0b9e31b29ebbb
Srujan Gurram <srujangs8@gmail.com>
2025-06-16 16:20:29 +0530

    Remove debug console.log from near-agent

--- src/agents/near-agent.ts
+++ src/agents/near-agent.ts
@@ -23,15 +23,6 @@ export const getNearAgent = async (samplingHandler?: SamplingHandler) => {
 		},
 	});
 
-	console.log({
-		ACCOUNT_ID: env.ACCOUNT_ID,
-		ACCOUNT_KEY: env.ACCOUNT_KEY,
-		NEAR_NETWORK_ID: env.NEAR_NETWORK_ID,
-		NEAR_NODE_URL: env.NEAR_NODE_URL,
-		NEAR_GAS_LIMIT: env.NEAR_GAS_LIMIT,
-		PATH: env.PATH,
-	});
-
 	const nearAgentTools = await nearAgentToolSet.getTools();
 
 	const agent = new Agent({
