90591e09bc9654f5891419831b68f6880aec50cc
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2025-07-31 11:57:38 +0530

    update

--- src/agents/amm-agent/agent.ts
+++ src/agents/amm-agent/agent.ts
@@ -1,14 +1,15 @@
 import { model } from "@/env";
 import { AgentBuilder } from "@iqai/adk";
-import { amountOutCalculator } from "./tools";
+import { AmountOutCalculatorTool } from "./tools";
 
 export const getAmmAgent = async () => {
+	const tool = new AmountOutCalculatorTool();
 	const agent = await AgentBuilder.create("amm_agent")
 		.withDescription(
 			"This agent performs automated market making calculations to determine optimal output amounts. You only respond with output amount. nothing else.",
 		)
 		.withModel(model)
-		.withTools(amountOutCalculator)
+		.withTools(tool)
 		.build();
 
 	return agent;
